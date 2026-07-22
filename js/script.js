24b08758cb198adce3c2282444f1f600f8b09e86
Jacques McDermid Heyns <160586536+Kuma1984@users.noreply.github.com>
2026-07-22 13:19:13 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,6 +1,10 @@
 /* ==========================================================
    Jacques McDermid Heyns Portfolio Website
    script.js
+
+   Note: Template used and adapted, so all functionality not
+   necessarily working. Can be updated and refined going
+   forward.
    ========================================================== */
 
 const titles = [
